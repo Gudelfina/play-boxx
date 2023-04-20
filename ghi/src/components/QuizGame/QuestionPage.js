e605fb5c062ab705e7eaa4715fbea914956f5897
Gudelfina Mendez <gudelfina.mendez@gmail.com>
2023-04-20 17:51:15 +0000

    #52 #11 Add QuestionPage to quiz game

---
+++ ghi/src/components/QuizGame/QuestionPage.js
@@ -0,0 +1,26 @@
+import QuestionSet from "./QuestionSet";
+import Timer from "../Timer";
+import { useSelector } from "react-redux";
+
+export default function QuestionPage(props) {
+  const score = useSelector((state) => state.quizgame.score);
+  const timer = useSelector((state) => state.quizgame.time_completed);
+
+  return (
+    <div className="component-container container ">
+      <QuestionSet
+        score={score}
+        setIsStarted={props.setIsStarted}
+        setIsEndOfGame={props.setIsEndOfGame}
+      />
+      Score: {score} <br />
+      Timer:{" "}
+      <Timer
+        isEndOfGame={props.isEndOfGame}
+        setIsEndOfGame={props.setIsEndOfGame}
+        isStarted={props.isStarted}
+      />
+      Redux State Timer: {timer}
+    </div>
+  );
+}
