ea40fb8ea78194e07908ca1951fe64328262311d
Sirasit Punnanithi <spunnanithi@gmail.com>
2023-04-26 17:43:57 +0000

    Moved BrowserRouter to App.js and fixed links

--- ghi/src/App.js
+++ ghi/src/App.js
@@ -1,6 +1,5 @@
 import "./App.css";
 import LoginForm from "./components/LoginForm";
-import { Routes, Route } from "react-router-dom";
 import GamePage from "./components/GamePage";
 import SignUpForm from "./components/SignUpForm";
 import NavBar from "./components/NavBar";
@@ -11,36 +10,57 @@ import { ProfilePage } from "./components/ProfilePage";
 import ProtectedRoute from "./components/ProtectedRoute";
 import CreatorPage from "./components/CreatorPage";
 import EditProfilePage from "./components/EditProfilePage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { PersistGate } from "redux-persist/integration/react";
+import { ToastContainer } from "react-toastify";
+import { persistor } from "./store/store";
 
 export default function App() {
-	return (
-		<>
-			<NavBar />
-			<Routes>
-				<Route path="/" element={<LandingPage />}></Route>
-				<Route path="/login" element={<LoginForm />}></Route>
-				<Route path="/signup" element={<SignUpForm />}></Route>
-				<Route path="/leaderboard" element={<LeaderBoard />}></Route>
-				<Route path="/games" element={<GamePage />}></Route>
-				<Route path="/games/quiz" element={<StartPage />}></Route>
-				<Route path="/creators" element={<CreatorPage />}></Route>
-				<Route
-					path="/profile"
-					element={
-						<ProtectedRoute>
-							<ProfilePage />
-						</ProtectedRoute>
-					}
-				/>
-				<Route
-					path="/profile/edit"
-					element={
-						<ProtectedRoute>
-							<EditProfilePage />
-						</ProtectedRoute>
-					}
-				/>
-			</Routes>
-		</>
-	);
+  const domain = /https:\/\/[^/]+/;
+  const basename = process.env.PUBLIC_URL.replace(domain, "");
+
+  return (
+    <BrowserRouter basename={basename}>
+      <PersistGate persistor={persistor}>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<LandingPage />}></Route>
+          <Route path="/login" element={<LoginForm />}></Route>
+          <Route path="/signup" element={<SignUpForm />}></Route>
+          <Route path="/leaderboard" element={<LeaderBoard />}></Route>
+          <Route path="/games" element={<GamePage />}></Route>
+          <Route path="/games/quiz" element={<StartPage />}></Route>
+          <Route path="/creators" element={<CreatorPage />}></Route>
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <ProfilePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/profile/edit"
+            element={
+              <ProtectedRoute>
+                <EditProfilePage />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+        <ToastContainer
+          position="bottom-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+      </PersistGate>
+    </BrowserRouter>
+  );
 }
--- ghi/src/index.js
+++ ghi/src/index.js
@@ -1,36 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import "./index.css";
+import ReactDOM from "react-dom/client";
 import App from "./App";
-import { store, persistor } from "./store/store";
+import { store } from "./store/store";
 import { Provider } from "react-redux";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { PersistGate } from "redux-persist/integration/react";
-import { ToastContainer } from "react-toastify";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const domain = /https:\/\/[^/]+/;
-const basename = process.env.PUBLIC_URL.replace(domain, "");
 root.render(
-	<Provider store={store}>
-		<BrowserRouter basename={basename}>
-			<PersistGate persistor={persistor}>
-				<Routes>
-					<Route path="/*" element={<App />}></Route>
-				</Routes>
-				<ToastContainer
-					position="bottom-right"
-					autoClose={5000}
-					hideProgressBar={false}
-					newestOnTop
-					closeOnClick
-					rtl={false}
-					pauseOnFocusLoss
-					draggable
-					pauseOnHover
-					theme="light"
-				/>
-			</PersistGate>
-		</BrowserRouter>
-	</Provider>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
